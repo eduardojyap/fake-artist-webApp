ecf390310fc782f2a88dac1740038410d8191b12
Eduardo Yap <eduardojyap@gmail.com>
2018-11-11 16:14:32 -0600

    added styles and loading page

--- src/components/ExpensesSummary.js
+++ src/components/ExpensesSummary.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import {connect} from 'react-redux';
+import {Link} from 'react-router-dom';
 import selectExpenses from '../selectors/expenses';
 import selectExpensesTotal from '../selectors/expenses-total'
 import numeral from 'numeral'
 
 export class ExpensesSummary extends React.Component {
     render() {
-        return <p>Viewing {this.props.expensesCount} expenses totalling {numeral(this.props.expensesTotal/100).format('$0,0.00')}</p>
+        return (
+            <div className="page-header">
+            <div className="content-container">
+                <h1 className="page-header__title">Viewing <span>{this.props.expensesCount} </span>expense{this.props.expensesCount !== 1 ? 's' : ''} totalling <span>{numeral(this.props.expensesTotal/100).format('$0,0.00')}</span></h1>
+                <div className="page-header__actions">
+                    <Link className="button"to="/create">Create Expense</Link>
+                </div>
+                </div>
+            </div>
+            )
     }
 }
 
