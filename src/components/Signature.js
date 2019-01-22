971cccbbac2e771c6de9eea9bed4681fbe6e86dd
Eduardo Yap <eduardojyap@gmail.com>
2019-01-21 19:05:59 -0600

    updated signature

--- src/components/Signature.js
+++ src/components/Signature.js
@@ -7,7 +7,6 @@ class Signature extends React.Component {
                 <div className="signature">
                     <p><a className="link" href="https://tesera.ru/images/items/744225/rule_fakeartist_e.pdf" target="_blank">Rules</a></p>
                     <p>Inspired by <a className="link" href="https://spyfall.crabhat.com" target="_blank">https://spyfall.crabhat.com/</a></p>
-                    <p><a className="link" href="https://github.com/ejyap/fake-artist-webApp" target="_blank">https://github.com/ejyap/fake-artist-webApp</a></p>
                 </div>    
             </div>
         )
